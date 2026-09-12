3054c3f0b92318677bc0c778a0edad5a0c6c297e
ruhulaminislam <abmdruhulamin@gmail.com>
2026-09-12 14:35:45 +0600

    Store data fetching Promise in state

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import { Navbar } from './Navbar'
 
 import './App.css'
 import { Hero } from './ Hero'
+import type { TechnologiesDataProps } from './TechnologiesData'
+import { useState } from 'react'
+const TechnologieDataPromic=async():Promise<TechnologiesDataProps[]>=>{
+  const rec=await fetch("/data.json")
+  const data=await rec.json()
+  return data
+}
+
 
 function App() {
-
+const [technologiesData]=useState(()=>TechnologieDataPromic())
 
   return (
     <>
